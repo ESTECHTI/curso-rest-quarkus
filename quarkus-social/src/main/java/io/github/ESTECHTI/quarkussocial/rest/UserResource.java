e96f3fb862b5a70116a718b0f841f3484219442e
ESTECHTI <e.s.tech@outlook.com>
2024-07-23 12:04:01 -0300

    Recebendo e devolvendo objetos JSON, finalizado.

--- quarkus-social/src/main/java/io/github/ESTECHTI/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/ESTECHTI/quarkussocial/rest/UserResource.java
@@ -1,15 +1,24 @@
 package io.github.ESTECHTI.quarkussocial.rest;
 
 import io.github.ESTECHTI.quarkussocial.rest.dto.CreateUserRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import javax.print.attribute.standard.Media;
+
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser( CreateUserRequest userRequest ) {
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
         return Response.ok().build();
     }
 
