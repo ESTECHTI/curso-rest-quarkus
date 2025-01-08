f9afee27c45cf7f25cd633e2fa6420939a2ff01b
ESTECHTI <e.s.tech@outlook.com>
2025-01-08 10:05:05 -0300

    Testes de listagem de seguidores finalizados

--- quarkus-social/src/test/java/io/github/ESTECHTI/quarkussocial/rest/FollowerResourceTest.java
+++ quarkus-social/src/test/java/io/github/ESTECHTI/quarkussocial/rest/FollowerResourceTest.java
@@ -1,6 +1,8 @@
 package io.github.ESTECHTI.quarkussocial.rest;
 
+import io.github.ESTECHTI.quarkussocial.domain.model.Follower;
 import io.github.ESTECHTI.quarkussocial.domain.model.User;
+import io.github.ESTECHTI.quarkussocial.domain.repository.FollowRespository;
 import io.github.ESTECHTI.quarkussocial.domain.repository.UserRespository;
 import io.github.ESTECHTI.quarkussocial.rest.dto.FollowerRequest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
@@ -10,12 +12,10 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 @TestHTTPEndpoint(FollowerResource.class)
@@ -23,6 +23,8 @@ class FollowerResourceTest {
 
     @Inject
     UserRespository userRespository;
+    @Inject
+    FollowRespository followRespository;
 
     Long userId;
     Long followerId;
@@ -43,6 +45,12 @@ class FollowerResourceTest {
         follower.setName("Cicrano");
         userRespository.persist(follower);
         followerId = follower.getId();
+
+        //cria um follower
+        var followerEntity = new Follower();
+        followerEntity.setFollower(follower);
+        followerEntity.setUser(user);
+        followRespository.persist(followerEntity);
     }
 
     @Test
@@ -64,8 +72,8 @@ class FollowerResourceTest {
     }
 
     @Test
-    @DisplayName("should return 404 when User id doesn't exist")
-    public void userNotFoundTest() {
+    @DisplayName("should return 404 on follow a user when User id doesn't exist")
+    public void userNotFoundWhenTryingToFollowTest() {
 
         var body = new FollowerRequest();
         body.setFollowerId(userId);
@@ -98,4 +106,39 @@ class FollowerResourceTest {
         .then()
             .statusCode(Response.Status.NO_CONTENT.getStatusCode());
     }
+
+    @Test
+    @DisplayName("should return 404 on list user followers and User id doesn't exist")
+    public void userNotFoundWhenListingFollowersTest() {
+        var inexistentUserId = 999;
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("userId", inexistentUserId)
+        .when()
+            .get()
+        .then()
+            .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("should list a user's followers")
+    public void listFollowersTest() {
+        var response =
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("userId", userId)
+        .when()
+            .get()
+        .then()
+            .extract().response();
+
+    var followersCount = response.jsonPath().get("followersCount");
+    var followersContent = response.jsonPath().getList("content");
+
+    assertEquals(Response.Status.OK.getStatusCode(), response.statusCode());
+    assertEquals(1, followersCount);
+    assertEquals(1, followersContent.size());
+    }
+
 }
